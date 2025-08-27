53effc8d30a06c8f0a6a4c3b2d7ca4585115681b
VENKAT6912 <vibecoder18@gmail.com>
2025-08-27 16:46:47 +0530

    UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Form from "./components/form";
 import './index.css';
 
-
 function App() {
   return (
-    <div classname="container">
-      <h1>Form with Validation </h1>
-      <Form />
+    <div className="container">
+      <div className="form-card">
+        <h1>Form with Validation</h1>
+        <Form />
+      </div>
     </div>
   );
 }
